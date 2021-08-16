cb054449a73a822a20e3fa4490b2eacc32de6357
jacob <jacobharv00@gmail.com>
2021-08-16 16:16:20 -0600

    commit tree e8dbce9b10358e007adb6dc0abf315ea0330fd5c

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :sessions, only: [:create, :destroy]
+  # wouldn't it be nice if the tests let me use line 3?
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#show"
 end
